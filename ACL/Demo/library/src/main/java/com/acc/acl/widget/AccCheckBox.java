1ea0d6bd6b357935e6f844c1a7667f6bbdda383e
nmbinh <binh.nguyenminh@apps-cyclone.com>
2016-03-22 14:49:01 +0700

    Implement AccCheckBox widget which can set custom font on xml source conveniently

---
+++ ACL/Demo/library/src/main/java/com/acc/acl/widget/AccCheckBox.java
@@ -0,0 +1,43 @@
+package com.acc.acl.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+
+import com.acc.acl.helper.FontLoader;
+
+/**
+ * Created by binhnguyen on 3/22/16.
+ * <p/>
+ * Extend built-in {@link CheckBox} with below features:
+ * - Developer is able to set custom font within custom attribute.
+ */
+public class AccCheckBox extends CheckBox {
+    public AccCheckBox(Context context) {
+        super(context);
+    }
+
+    public AccCheckBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        loadCustomFont(context,attrs);
+    }
+
+    public AccCheckBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        loadCustomFont(context,attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AccCheckBox(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        loadCustomFont(context,attrs);
+    }
+
+    private void loadCustomFont(Context context, AttributeSet attrs) {
+        Typeface typeface = FontLoader.extractTypeface(context, attrs);
+        setTypeface(typeface);
+    }
+}
